22fdc6470f52e6d9878b474aa915f811e8ca4674
vivekduggaraju <105599859+vivekduggaraju@users.noreply.github.com>
2024-09-23 13:45:55 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -40,7 +40,7 @@ export default function Header() {
                               layoutId="activeSection"
                               transition={{
                               type:"spring",
-                              stiffness: "380",
+                              stiffness: 380,
                               damping:30,
                               }}>
                             </motion.span>)}
