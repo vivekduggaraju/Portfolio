9f4c3cb72d3b7a4abb46d7c18b32946116d95865
vivekduggaraju <105599859+vivekduggaraju@users.noreply.github.com>
2024-09-23 14:26:42 -0400

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -20,7 +20,7 @@ const{ref }=useSectionInView("About",0.75);
         id="about">
         <SectionHeading >About Me</SectionHeading>
         <p className='mb-4'> 
-          &emsp; I am a dedicated and passionate computer science student currently pursuing a BS in Computer Science at Georgia State University, where I have maintained a good academic stand and earned recognition on the President's List for multiple semesters. My academic journey is deeply rooted in my love for technology and problem-solving. Beyond the classroom, I actively engage in professional development opportunities through internships and collaborative projects.
+          &emsp; I am a dedicated and passionate computer science student currently pursuing a BS in Computer Science at Georgia State University, where I have maintained a good academic standing and earned recognition on the President's List for multiple semesters. My academic journey is deeply rooted in my love for technology and problem-solving. Beyond the classroom, I actively engage in professional development opportunities through internships and collaborative projects.
         </p>
         <p>
           &emsp; In addition to my technical expertise, I am highly adaptable, driven by a desire for continuous learning and growth. Whether working on a team or independently, I bring a strong work ethic, effective communication, and problem-solving abilities to every challenge I take on​.
