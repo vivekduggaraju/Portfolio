d2c351f5ebed7989757b2b3954c4a99cceada216
vivekduggaraju <105599859+vivekduggaraju@users.noreply.github.com>
2024-10-02 10:17:22 -0400

    Update section_divider.tsx

--- components/section_divider.tsx
+++ components/section_divider.tsx
@@ -9,7 +9,7 @@ export default function SectionDivider() {
   return (
     <motion.div  style={{ 
       position: "relative",
-      width:'100%', height:'42rem',
+      width:'70%', height:'30rem',
 
 
       transform:  'translate(0, 100 %)',
